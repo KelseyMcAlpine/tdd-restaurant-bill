621fcc3a07314cf468ac2e3a4812be471ac0be71
Kelsey McAlpine <kmcalpine023@gmail.com>
2017-02-27 12:07:40 -0800

    Created tests for order_item, bill_with_tax, tip, and final_bill methods in restaurant_bill.rb

--- specs/restaurant_bill_spec.rb
+++ specs/restaurant_bill_spec.rb
@@ -2,3 +2,52 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 require 'minitest/skip_dsl'
 require_relative '../lib/restaurant_bill'
+
+Minitest::Reporters.use!
+Minitest::Reporters::SpecReporter.new
+
+describe RestaurantBill do
+
+  before do
+    @my_bill = RestaurantBill.new
+  end
+
+  it "A bill is created with an empty ordered_items" do
+    @my_bill.ordered_items.must_be_instance_of Array
+    @my_bill.ordered_items.length.must_equal 0
+  end
+
+  it "Should add item to ordered_items" do
+    @my_bill.order_item("pizza", 10)
+    @my_bill.ordered_items.length.must_equal 1
+  end
+
+  it "Should add multiple items to ordered items" do
+    3.times do
+      @my_bill.order_item("pizza", 10)
+    end
+
+    @my_bill.ordered_items.length.must_equal 3
+  end
+
+  it "Should return total of all items plus tax" do
+    3.times do
+      @my_bill.order_item("pizza", 10)
+    end
+
+    @my_bill.bill_with_tax.must_equal 32.7
+  end
+
+  it "Should allow user to add a tip" dos
+    @my_bill.order_item("pizza", 10)
+
+    @my_bill.tip(20).must_equal 13.08
+  end
+
+  it "Should return final bill" do
+    @my_bill.order_item("pizza", 10)
+    @my_bill.tip(20)
+    @my_bill.final_bill.must_equal "SUBTOTAL: 10.0\nTAX: 0.9\nTIP: 2.18\nTOTAL: 13.08"
+  end
+
+end
